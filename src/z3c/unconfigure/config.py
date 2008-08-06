a98b8c3c02d4e6fab4f5649b9029aa9f0340c7f3
Philipp von Weitershausen <philipp@weitershausen.de>
2008-08-06 08:25:24 +0000

    license headers
    git-svn-id: svn+ssh://fridge/stuff/zope-mirror/z3c.unconfigure/trunk@89425 62d5b8a3-27da-0310-9561-8e5933582275

--- src/z3c/unconfigure/config.py
+++ src/z3c/unconfigure/config.py
@@ -1,3 +1,18 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""The 'unconfigure' grouping directive
+"""
 from zope.configuration.zopeconfigure import ZopeConfigure
 
 class Unconfigure(ZopeConfigure):
