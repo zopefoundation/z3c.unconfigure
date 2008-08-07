dad59b24dbbcc3fa2f492f3c7d9a1cd13673d08d
Philipp von Weitershausen <philipp@weitershausen.de>
2008-08-07 09:18:45 +0000

    Remove dev marker
    git-svn-id: svn+ssh://fridge/stuff/zope-mirror/z3c.unconfigure/tags/1.0@89488 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (
 
 setup(
     name='z3c.unconfigure',
-    version='1.0dev',
+    version='1.0',
     description=("Disable specific ZCML directives in other package's "
                  "configuration"),
     long_description=long_description,
