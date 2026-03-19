088d3d99e699677f0feccbcd985eadca4f98b973
Michael Howitz <icemac@gmx.net>
2026-03-19 08:37:09 +0100

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,50 +1,5 @@
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-
-long_description = (
-    "Documentation\n"
-    + "=============\n\n"
-    + read("src", "z3c", "unconfigure", "README.rst")
-    + "\n"
-    + read("CHANGES.rst")
-)
-
-setup(
-    name="z3c.unconfigure",
-    version="3.1.dev0",
-    description=(
-        "Disable specific ZCML directives in other package's configuration"),
-    long_description=long_description,
-    author="Philipp von Weitershausen",
-    author_email="zope-dev@zope.dev",
-    url="https://github.com/zopefoundation/z3c.unconfigure",
-    license="ZPL",
-    classifiers=[
-        "Programming Language :: Python",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Zope Public License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-    ],
-    include_package_data=True,
-    zip_safe=False,
-    python_requires=">=3.10",
-    install_requires=[
-        "setuptools",
-        "zope.configuration >= 3.8.0",
-        "zope.component",  # technically [zcml]
-        "zope.security",
-        "zope.event",
-        "zope.testing",
-    ],
-)
+# See pyproject.toml for package metadata
+setup()
